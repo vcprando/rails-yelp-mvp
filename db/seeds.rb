080c1d1079f59795389413fddf00817f04570636
Valeria Prando <valeria.cprando@gmail.com>
2018-03-28 12:05:59 -0300

    Create models restaurant and review, create the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# db/seeds.rb
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number:  '11 56789090',
+    category:        'chinese'
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number:  '11 56739090',
+    category:         'italian'
+  }
+]
+
+restaurants_attributes.each do |restaurant|
+  Restaurant.create!(restaurant)
+end
+
