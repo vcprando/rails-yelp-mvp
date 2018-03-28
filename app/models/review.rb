12b0b73ed46723aa681de7cab0654af3a511b733
Valeria Prando <valeria.cprando@gmail.com>
2018-03-28 12:32:50 -0300

    Create models validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
-
+  validates :content, presence: true
+  validates :rating, inclusion: { in: [0, 1, 2, 3, 4, 5] }
+  validates :rating, numericality: {only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 5}
 end
